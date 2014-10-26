df2490559645f6f25b2981a58f90721160f58efc
Juliusername <juliusodb@gmail.com>
2014-10-26 16:46:04 +0100

    databaseMediator Save()

--- StamboomProject/StamboomProject/src/stamboom/storage/DatabaseMediator.java
+++ StamboomProject/StamboomProject/src/stamboom/storage/DatabaseMediator.java
@@ -6,9 +6,13 @@ package stamboom.storage;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 import stamboom.domain.Administratie;
+import stamboom.domain.Gezin;
+import stamboom.domain.Persoon;
 
 public class DatabaseMediator implements IStorageMediator {
 
@@ -24,7 +28,103 @@ public class DatabaseMediator implements IStorageMediator {
 
     @Override
     public void save(Administratie admin) throws IOException {
-        //todo opgave 4     
+        //todo opgave 4
+        if (this.isCorrectlyConfigured() == false)
+        {
+            throw new IOException("Incorectly configured");
+        }
+        
+        try
+        {
+            //Initialize
+            this.initConnection();
+            
+            //Delete everything
+            Statement deletePersonen = conn.createStatement();
+            Statement deleteGezinnen = conn.createStatement();
+            
+            deletePersonen.execute("DELETE * FROM personen");
+            deleteGezinnen.execute("DELETE * FROM gezinnen");
+            
+            //Insert
+            Statement insertPersoon = conn.createStatement();
+            String baseInsertPersoonQuery = "INSERT INTO personen (persoonsNummer, achternaam, voornamen, tussenvoegsel, geboortedatum, geboorteplaats, geslacht, ouders) VALUES (";
+            String insertPersoonQuery = "";
+            
+            for(Persoon p : admin.getPersonen())
+            {
+                insertPersoonQuery = baseInsertPersoonQuery;
+                insertPersoonQuery += p.getNr() + ", ";
+                insertPersoonQuery += p.getAchternaam() + ", ";
+                insertPersoonQuery += p.getVoornamen() + ", ";
+                insertPersoonQuery += p.getTussenvoegsel() + ", ";
+                insertPersoonQuery += p.getGebDat() + ", ";
+                insertPersoonQuery += p.getGebPlaats() + ", ";
+                insertPersoonQuery += p.getGeslacht() + ", ";
+                
+                if (p.getOuderlijkGezin() == null)
+                {
+                    insertPersoonQuery += "null";
+                }
+                else
+                {
+                    insertPersoonQuery += p.getOuderlijkGezin().getNr();
+                }
+                
+                insertPersoonQuery += ")";
+                
+                insertPersoon.execute(insertPersoonQuery);
+            }
+            
+            //Insert
+            Statement insertGezin = conn.createStatement();
+            String baseInsertGezinQuery = "INSERT INTO gezinnen (gezinsNummer, ouder1, ouder2, huwelijksdatum, scheidingsdatum) VALUES (";
+            String insertGezinQuery = "";
+            
+            for(Gezin g : admin.getGezinnen())
+            {
+                insertGezinQuery = baseInsertGezinQuery;
+                insertGezinQuery += g.getNr() + ", ";
+                insertGezinQuery += g.getOuder1().getNr() + ", ";
+                
+                if (g.getOuder2() == null)
+                {
+                    insertGezinQuery += "null" + ", ";
+                }
+                else
+                {
+                    insertGezinQuery += g.getOuder2().getNr() + ", ";
+                }
+                
+                if (g.getHuwelijksdatum() == null)
+                {
+                    insertGezinQuery += "null" + ", ";
+                }
+                else
+                {
+                    insertGezinQuery += g.getHuwelijksdatum() + ", ";
+                }
+                
+                if (g.getHuwelijksdatum() == null)
+                {
+                    insertGezinQuery += "null";
+                }
+                else
+                {
+                    insertGezinQuery += g.getScheidingsdatum();
+                }
+                
+                
+                insertGezinQuery += ")";
+                
+                insertGezin.execute(insertGezinQuery);
+            }
+            
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Exception: " + ex.getMessage());
+        }
     }
 
     @Override
@@ -70,6 +170,27 @@ public class DatabaseMediator implements IStorageMediator {
 
     private void initConnection() throws SQLException {
         //opgave 4
+        String url = "";
+        String username = "";
+        String password = "";
+        
+        try
+        {
+            url = this.props.getProperty(url);
+            username = this.props.getProperty(username);
+            password = this.props.getProperty(password);
+            
+            if (!url.equals("") && !username.equals("") && !password.equals(""))
+            {
+                this.conn = DriverManager.getConnection(url, username, password);
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+        
+        
     }
 
     private void closeConnection() {
